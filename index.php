97808f359ff9663ef0c2dae607a2ba4bc1cb3164
calonso-conabio <calonso@conabio.gob.mx>
2015-10-23 13:41:53 -0500

    la bilbioteca de yii, ahora dentro de protected/vendors

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-//defined('YII_DEBUG', true);
+defined('YII_DEBUG', true);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 set_time_limit(10000);
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii-1.1.13.e9e4a0/framework/yii.php';
+$yii=dirname(__FILE__).'/protected/vendors/yii-1.1.13.e9e4a0/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
